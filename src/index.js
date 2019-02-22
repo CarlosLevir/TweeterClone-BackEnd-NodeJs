e8d4670aa4eec4de8349069a1c3c1a97b55e0aac
Carlos Levir Freire de Souza <carlos.levir@secrelnet.local>
2019-02-22 11:44:56 -0300

    Add of Routes.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-mongoose.connect('mongodb://carloslevir:abc123456@ds127015.mlab.com:27015/goweek-carloslevir', 
+const { username, password, database } = require('./env.json');
+
+mongoose.connect('mongodb://' + username + ':' + password + database, 
   {
     useNewUrlParser: true
   }
 );
 
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
+app.use(cors());
 app.use(express.json());
 app.use(require('./routes'));
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000')
+server.listen(3001, () => {
+  console.log('Server started on port 3001')
 });
